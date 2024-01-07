97a27dbc6d927a2526ad585496d680ae52d28f65
Михаил Коновалов <km1444@yandex.ru>
2024-01-07 12:29:11 +0300

    add goalkeeper_app

--- hockey/hockey/settings.py
+++ hockey/hockey/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'sorl.thumbnail',
     'miscellaneous.apps.MiscellaneousConfig',
+    'goalkeeper_app.apps.GoalkeeperAppConfig',
 ]
 
 MIDDLEWARE = [
